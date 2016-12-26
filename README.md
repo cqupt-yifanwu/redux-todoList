987f926c9c30ba38871b51c1e99ef13c807b4c16
cqupt-yifanwu <616584789@qq.com>
2016-12-26 10:52:25 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # redux-todoList
 根据redux官方文档做的一个demo
+####初始redux（为了解决组件间的管理和状态的保存）
+*按照官方文档的指导使用webpack和es6的语法构建
+#### npm install
+#### npm start
+#### 访问localhost:8080
--- app/components/Todo.js
+++ app/components/Todo.js
@@ -3,7 +3,10 @@ import React, { Component, PropTypes } from 'react'
 export default class Todo extends Component {
   render() {
     const { onClick, completed, text } = this.props;
-    return <li onClick={onClick} style={{textDecoration: completed ? 'line-through' : 'none', cursor: completed ? 'default' : 'pointer'}}> 
+    return <li 
+    onClick={onClick} 
+    style={{textDecoration: completed ? 'line-through' : 'none',
+    cursor: completed ? 'default' : 'pointer'}}> 
       {text}
     </li>;
   }
